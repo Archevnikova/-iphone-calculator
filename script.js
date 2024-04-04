ee2c22308b93ec867f036d8a3f73ea1af7d797fc
Archevnikova <ms.orlovakatya@mail.ru>
2024-04-04 18:09:06 +0300

    add number jquery

---
+++ script.js
@@ -0,0 +1,82 @@
+$(".digit7").click(function(){
+    const prefVal = $(".result__answer").val();
+    $(".result__answer").val(`${prefVal}7`);
+    
+})
+$(".digit8").click(function(){
+    $(".result__answer").val("8");
+    
+})
+$(".digit9").click(function(){
+    $(".result__answer").val("9");
+    
+})
+$(".digit4").click(function(){
+    $(".result__answer").val("4");
+    
+})
+
+$(".digit5").click(function(){
+    $(".result__answer").val("5");
+    
+})
+
+$(".digit6").click(function(){
+    $(".result__answer").val("6");
+    
+})
+$(".digit1").click(function(){
+    $(".result__answer").val("1");
+    
+})
+$(".digit2").click(function(){
+    $(".result__answer").val("2");
+    
+})
+$(".digit3").click(function(){
+    $(".result__answer").val("3");
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btn = document.querySelector(".buttons");
+// const result=document.querySelector("#result");
+// btn.addEventListener("click", function(event){
+//     console.log(event.target);
+//     if (!event.target.classList.contains("button"))return
+// const value= event.target.innerText;
+
+// switch(value){
+//     case "AC":
+//         result.innerText = "";
+//         break;
+//     case "=":
+//         result.innerText = eval(result.innerText).toFixed(2);
+//         break;
+//     default:
+//         result.innerText+= value;
+// }
+
+
+// });
