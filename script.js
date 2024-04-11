2baf8530d5c71cf57ca62eecc42c00582d79818e
Musin Dinislam <MusinDi@inmeta.ru>
2024-04-11 22:01:34 +0500

    [feat]: added function on native js

--- script.js
+++ script.js
@@ -1,183 +1,28 @@
-// function currentValue() {
-//     return $("#result");
-// }
 let currentValue = "";
 let operation = "";
 
-// проходимся в цикле по каждой кнопке с числом, 
-// и добавляем слушатель на пересчет результата
-for(let i = 1; i < 10; i++) {
+function addListenersToDigits() {
 
-    $(`#${i}`).click(function() {
+	for(let i = 1; i < 10; i++) {
+		const selector = `${i}`;
 
-        const cv = $("#result").val(); //111
-    
-        const result = `${cv}${i}`;
-    
-        $("#result").val(result);
-    
-    });
-    
+		const button = document.getElementById(selector);
+
+		button.addEventListener("click", function() {
+			const cvInput = document.querySelector("#result");
+			const valInput = cvInput.value;
+			const result = `${valInput}${i}`;
+			cvInput.value = result;
+		});
+	}
 }
 
-// вешаеем слушатель на очистку результата
-$("#ac").click(function(){
-    $("#result").val("");
-});
-
-// вешаеем слушатель на =
-$("#eq").click(function(){
-    let result = 0;
-    if (operation === "+"){
-        result = Number(currentValue) + Number($("#result").val());
-    } else if (operation === "-"){
-        result = Number(currentValue) - Number($("#result").val());
-    }else if (operation === "/"){
-        result = Number(currentValue) / Number($("#result").val());
-    }else if (operation === "*"){
-        result = Number(currentValue) * Number($("#result").val());
-    }
-    $("#plus,#minus,#division,#mul").css({backgroundColor:"",color:""});
-    $("#result").val(result);
-
-});
+addListenersToDigits();
 
 
-// вешаеем слушатель на +
-$("#plus").click(function(){
-    currentValue = $("#result").val();
-    operation = "+";
-    $("#plus").css({backgroundColor:"white",color:"black"});
-    $("#result").val("");
-    
-});
+const acElement = document.getElementById("ac");
 
-
-// вешаеем слушатель на -
-$("#minus").click(function(){
-    currentValue = $("#result").val();
-    operation = "-";
-    $("#result").val("");
-    $("#minus").css({backgroundColor:"white",color:"black"});
-});
-
-
-// вешаеем слушатель на /
-$("#division").click(function(){
-    operation = "/";
-    currentValue = $("#result").val();
-    $("#result").val("");
-    $("#division").css({backgroundColor:"white",color:"black"});
-});
-
-
-// вешаеем слушатель на *
-$("#mul").click(function(){
-    currentValue = $("#result").val();
-    operation = "*";
-    $("#result").val("");
-    $("#mul").css({backgroundColor:"white",color:"black"});
-});
-
-$("#invert").click(function(){
-    currentValue = $("#result").val();
-    currentValue = -currentValue;
-    $("#result").val(currentValue);
+acElement.addEventListener("click", function() {
+	const cvInput = document.querySelector("#result");
+	cvInput.value = "";
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(".digit7").click(function(){
-//     const prefVal = $(".result__answer").val();
-//     $(".result__answer").val(`${prefVal}7`);
-    
-// })
-
-// $(".digit8").click(function(){
-//     $(".result__answer").val("8");
-    
-// })
-// $(".digit9").click(function(){
-//     $(".result__answer").val("9");
-    
-// })
-// $(".digit4").click(function(){
-//     $(".result__answer").val("4");
-    
-// })
-
-// $(".digit5").click(function(){
-//     $(".result__answer").val("5");
-    
-// })
-
-// $(".digit6").click(function(){
-//     $(".result__answer").val("6");
-    
-// })
-// $(".digit1").click(function(){
-//     $(".result__answer").val("1");
-    
-// })
-// $(".digit2").click(function(){
-//     $(".result__answer").val("2");
-    
-// })
-// $(".digit3").click(function(){
-//     $(".result__answer").val("3");
-    
-// })
-
-
-
-// // const btn = document.querySelector(".buttons");
-// // const result=document.querySelector("#result");
-// // btn.addEventListener("click", function(event){
-// //     console.log(event.target);
-// //     if (!event.target.classList.contains("button"))return
-// // const value= event.target.innerText;
-
-// // switch(value){
-// //     case "AC":
-// //         result.innerText = "";
-// //         break;
-// //     case "=":
-// //         result.innerText = eval(result.innerText).toFixed(2);
-// //         break;
-// //     default:
-// //         result.innerText+= value;
-// // }
-
-
-// // });
